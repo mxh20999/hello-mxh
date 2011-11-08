6fe7e07dee4e25c4e73f4d0d724d28093124651d
Freedomxura <mxh20999@163.com>
2011-11-08 14:47:49 +0800

    It is just test egit in eclipse.

---
+++ Test git/src/Test git.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : Test.c
+ Author      : Ericteck http://xurafreedom.cublog.cn
+ Version     :
+ Copyright   : Ericteck
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("Hello World!"); /* prints Hello World! */
+	return EXIT_SUCCESS;
+}
